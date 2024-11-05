1acddbd9df0a3a32d33442575e9ce8ee278e7063
Recon1991 <kalanicook@yahoomail>
2024-11-04 17:44:48 -1000

    added license to source code files

--- cobblemon_spawndata_processor_v2.2.py
+++ cobblemon_spawndata_processor_v2.2.py
@@ -1,6 +1,9 @@
 # Editor: Recon1991 and Contributors
 # Project: Cobblemon Spawn Data Extractor
 # Description: This script extracts data from cobblemon JSON files for analysis.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import asyncio
 import aiofiles
--- column_names.py
+++ column_names.py
@@ -1,5 +1,9 @@
 # column_names.py
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 column_names = [
     "Dex Number",              # Pokémon's Dex number
     "Pokemon Name",            # Pokémon's name
